730952cec9c8028a55ad30a46833d7e8c3c9b331
Coop <cooperschwarting@icloud.com>
2024-01-28 14:08:14 -0600

    eeeeeeeeee

--- src/profile.cpp
+++ src/profile.cpp
@@ -5,18 +5,13 @@
 using namespace geode::prelude;
 
 class $modify(ProfilePage) {
-	/*static void onModify(auto& self) {
-			self.setHookPriority("ProfilePage::init", -1); // try to fix with BetterInfo
-	}*/
+	// TODO: add non betterinfo way back later
 
 	 void loadPageFromUserInfo(GJUserScore* user) {
-        //if (!ProfilePage::loadPageFromUserInfo(user)) return false;
 
 			auto value = Mod::get()->getSettingValue<std::string>("thename");
-    		// m_usernameLabel->setString(value.c_str());
 			if (Loader::get()->isModLoaded("cvolton.betterinfo")) {
 				static_cast<cocos2d::CCLabelBMFont*>(this->getChildByID("cvolton.betterinfo/username-button")->getChildren()->objectAtIndex(0))->setString(value.c_str()); //honestly i have no clue lol
-			}
-		return true
-	}
+			};
+	};
 };
