ccc92e58c41df12b37eedd54b303283fe8aa6c30
Coop <cooperschwarting@icloud.com>
2024-01-28 13:52:05 -0600

    eeeeeee

--- src/profile.cpp
+++ src/profile.cpp
@@ -2,8 +2,6 @@
 #include <Geode/modify/ProfilePage.hpp>
 #include <Geode/binding/ProfilePage.hpp>
 
-static geode::Loader* get();
-
 using namespace geode::prelude;
 
 class $modify(ProfilePage) {
@@ -12,13 +10,13 @@ class $modify(ProfilePage) {
 	}*/
 
 	 void loadPageFromUserInfo(GJUserScore* user) {
-        // if (!ProfilePage::init(accountID, ownProfile)) return false;
+        if (!ProfilePage::loadPageFromUserInfo(user)) return false;
 
 			auto value = Mod::get()->getSettingValue<std::string>("thename");
     		// m_usernameLabel->setString(value.c_str());
 			if (Loader::get()->isModLoaded("cvolton.betterinfo")) {
-				typeinfo_cast<cocos2d::CCLabelBMFont*>(this->getChildByID("cvolton.betterinfo/username-button")->getChildren()->objectAtIndex(0))->setString(value.c_str()); //honestly i have no clue lol
+				static_cast<cocos2d::CCLabelBMFont*>(this->getChildByID("cvolton.betterinfo/username-button")->getChildren()->objectAtIndex(0))->setString(value.c_str()); //honestly i have no clue lol
 			}
-		
+		return true
 	}
 };
