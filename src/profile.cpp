2c3b4a4fb7c3ad5dea92e7ac76ba0d1dc04ba889
Coop <cooperschwarting@icloud.com>
2024-01-28 13:35:55 -0600

    PLEASE FIX

--- src/profile.cpp
+++ src/profile.cpp
@@ -7,22 +7,18 @@ static geode::Loader* get();
 using namespace geode::prelude;
 
 class $modify(ProfilePage) {
-	static void onModify(auto& self) {
+	/*static void onModify(auto& self) {
 			self.setHookPriority("ProfilePage::init", -1); // try to fix with BetterInfo
-	}
+	}*/
 
-	 bool init(int accountID, bool ownProfile) {
-        if (!ProfilePage::init(accountID, ownProfile)) return false;
+	 void loadPageFromUserInfo(GJUserScore* user) {
+        // if (!ProfilePage::init(accountID, ownProfile)) return false;
 
-		if (ownProfile)
-		{
 			auto value = Mod::get()->getSettingValue<std::string>("thename");
-    		m_usernameLabel->setString(value.c_str());
+    		// m_usernameLabel->setString(value.c_str());
 			if (Loader::get()->isModLoaded("cvolton.betterinfo")) {
-				typeinfo_cast<cocos2d::CCLabelBMFont*>(this->getChildByID("cvolton.betterinfo/username-button")->getChildren()->objectAtIndex(1))->setString(value.c_str()); //honestly i have no clue lol
+				typeinfo_cast<cocos2d::CCLabelBMFont*>(this->getChildByID("cvolton.betterinfo/username-button")->getChildren()->objectAtIndex(0))->setString(value.c_str()); //honestly i have no clue lol
 			}
-		}
 		
-		return true;
 	}
 };
