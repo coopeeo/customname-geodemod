670e310e9dafe399b60da99c0f1ceba289436e7e
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-02-17 12:29:30 -0600

    DEBUG THIS!

--- src/levelinfo.cpp
+++ src/levelinfo.cpp
@@ -21,9 +21,10 @@ class $modify(LevelInfoLayer) {
         
         auto obj = static_cast<cocos2d::CCLabelBMFont*>(this->getChildByID("creator-info-menu")->getChildByID("creator-name")->getChildren()->objectAtIndex(0));
         std::string objString(obj->getString());
-        
+        auto accountname = static_cast<std::string>(GJAccountManager::get()->m_username);
         // log for debug
         log::info("Username of Level Creator is: [start]{}[end]", objString.substr(3,-1));
+        log::info("Username of Current Account is: [start]{}[end]", accountname);
 
         if(static_cast<std::string>(GJAccountManager::get()->m_username) != objString.substr(0,3)) 
             log::info("didnt pass username check");
