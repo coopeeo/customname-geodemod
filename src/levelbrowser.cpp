c90d83892cae9966bec9ef8d1b341928a68413c6
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-02-17 13:55:31 -0600

    Added Level Browser Layer

--- src/levelbrowser.cpp
+++ src/levelbrowser.cpp
@@ -8,29 +8,33 @@ class $modify(LevelBrowserLayer) {
     
     
 
-	bool init(GJGameLevel* p0, bool p1) {
-		if(!LevelBrowserLayer::init(p0, p1))
-			return false;
+	void loadLevelsFinished(cocos2d::CCArray* p0,const char* p1, int p2) {
+	    LevelBrowserLayer::loadLevelsFinished(p0, p1, p2);
         
-        
-        auto theContent = this->getChildren()->objectAtIndex(2)->getChildren()->objectAtIndex(0)->getChildren()->objectAtIndex(0)->getChildren()->objectAtIndex(0);
-        auto theContentChildren = theContent->getChildren();
-        for (size_t i = 0; i < theContent->getChildrenCount(); i++)
+        auto theContent = p0;
+        // auto theContent3 = static_cast<cocos2d::CCNode*>(this->getChildren()->objectAtIndex(2));
+        // auto theContent2 = static_cast<cocos2d::CCNode*>(theContent3->getChildren()->objectAtIndex(0));
+        // auto theContent1 = static_cast<cocos2d::CCNode*>(theContent2->getChildren()->objectAtIndex(0));
+        // auto theContent = static_cast<cocos2d::CCNode*>(theContent1->getChildren()->objectAtIndex(0));
+        auto theContentChildren = p0;
+        for (size_t i = 0; i < theContentChildren->count(); i++)
         {
-            auto mainObj = theContentChildren->objectAtIndex(i)->getChildByID("main-layer");
+            auto mainObj = static_cast<CCNode*>(theContentChildren->objectAtIndex(i))->getChildByID("main-layer");
 
-            auto obj = static_cast<cocos2d::CCLabelBMFont*>(mainObj->getChildByID("main-menu")->getChildByID("creator-name")->getChildren()->objectAtIndex(0));
-            std::string objString(obj->getString());
-            auto accountname = static_cast<std::string>(GJAccountManager::get()->m_username);
+            auto creatorname = mainObj->getChildByID("main-menu")->getChildByID("creator-name");
+            if (creatorname != nullptr) {
+                auto obj = static_cast<cocos2d::CCLabelBMFont*>(creatorname->getChildren()->objectAtIndex(0));
+                std::string objString(obj->getString());
+                auto accountname = static_cast<std::string>(GJAccountManager::get()->m_username);
 
-            if(static_cast<std::string>(GJAccountManager::get()->m_username) != objString.substr(3,-1)) return;
+                if(static_cast<std::string>(GJAccountManager::get()->m_username) == objString.substr(3,-1)){
+                    auto value = Mod::get()->getSettingValue<std::string>("thename");
 
-            auto value = Mod::get()->getSettingValue<std::string>("thename");
+                    obj->setString(fmt::format("By {}", value).c_str());
+                }
 
-            obj->setString(fmt::format("By {}", value).c_str());
+            }
             
         }
-        
-        return true;
 	}
 };
