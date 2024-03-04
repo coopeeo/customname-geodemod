4de5b5e225c1c47a7b77a662e6addc027784fe58
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-03-03 20:29:56 -0600

    sfsdafsadf

--- src/infolayer.cpp
+++ src/infolayer.cpp
@@ -20,9 +20,7 @@ class $modify(InfoLayer) {
         if (profilename != nullptr) {
             std::string objString(profilename->getString());
             auto accountname = static_cast<std::string>(GJAccountManager::get()->m_username);
-            log::debug("{}",objString.substr(0,objString.length()));
-            log::debug("{}",objString.length());
-            if(accountname == objString.substr(0,objString.length())){
+            if(accountname == objString.substr(0,accountname.length())){
 
                 auto value = Mod::get()->getSettingValue<std::string>("thename");
 
