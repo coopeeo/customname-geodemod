51a1741f6a8aa89648812444b8acfacd4ae369bf
Cooper <73203995+coopeeo@users.noreply.github.com>
2024-05-01 14:03:34 -0500

    oops... fix stuff in utils.hpp

--- src/utils.hpp
+++ src/utils.hpp
@@ -7,12 +7,11 @@ private:
     static Utils* sharedutils;
 public:
     Utils();
-    Utils() {};
 
     static Utils* get();
 
     bool const& getLaunch() const {return gamelaunch;}
-    void const& afterLaunch() {gamelaunch = false;}
+    void afterLaunch() {gamelaunch = false;}
     bool const& dnInstalled() const;
     bool const& nameTransferred() const;
 };
